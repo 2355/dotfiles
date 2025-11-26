f44b6efa624dac1d83f2c5f27228622d4bfe86d6
2355 <mitkit.a01@gmail.com>
2025-11-26 12:46:17 +0900

    update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,23 +5,25 @@
 # Finder
 #----------------------------------------------------------
 # 全ての拡張子のファイルを表示する
-defaults write NSGlobalDomain AppleShowAllExtensions 1
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # デフォルトで隠しファイルを表示する
-defaults write com.apple.finder AppleShowAllFiles 1
+defaults write com.apple.finder AppleShowAllFiles -bool true
 # 名前順でソートするときにフォルダを常に上部に表示
-defaults write com.apple.finder _FXSortFoldersFirst 1
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
 # 下部にパスバーを表示する
-defaults write com.apple.finder ShowPathbar 1
+defaults write com.apple.finder ShowPathbar -bool true
 
 #----------------------------------------------------------
 # Keyboard
 #----------------------------------------------------------
 # キーリピートの速度を最速にする
-defaults write NSGlobalDomain KeyRepeat 1
+defaults write NSGlobalDomain KeyRepeat -int  1
 # キーリピート開始までのタイミングを短くする
-defaults write NSGlobalDomain InitialKeyRepeat 15
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 # タップでクリックを有効にする
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad -bool true
+# F1、F2 などのキーを標準のファンクションキーとして使用する
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
 
 #----------------------------------------------------------
 # Dock
@@ -29,7 +31,7 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking 1
 # すべてのアプリアイコンを Dock から消去する
 defaults write com.apple.dock persistent-apps -array
 # Dock を自動的に隠す
-defaults write com.apple.dock autohide 1
+defaults write com.apple.dock autohide -bool true
 # Dock 表示の遅延時間を変更する
 defaults write com.apple.dock autohide-delay -float 0.1
 
